f83c8651b7a0e7ebea578b8809f848028b574adb
SaltySkyPie <danikh100@email.cz>
2022-02-25 19:48:46 +0100

    updated stats command

--- commands/stats.js
+++ commands/stats.js
@@ -5,12 +5,13 @@ module.exports = {
     description: 'Basic stats command',
     async execute(message, args, BotClient, functions) {
 
-        const [guilds, economy_wallet, economy_bank, users] = await Promise.all([
+        const [guilds, economy_wallet, economy_bank, economy_max_bank, users] = await Promise.all([
             functions.getServerCount(BotClient).catch(() => {return "loading..."}),
             functions.SQL("SELECT SUM(points) AS sumpoints FROM users"),
             functions.SQL("SELECT SUM(stash) AS sumpoints FROM users"),
+            functions.SQL("SELECT SUM(max_stash) AS sumpoints FROM users"),
             functions.SQL("SELECT COUNT(id) AS countusers FROM users")
         ])
-        message.reply(`Soap BOT is in **${guilds}** servers with **${users[0].countusers.toLocaleString()}** users. Total economy value is 🧼**${(economy_wallet[0].sumpoints + economy_bank[0].sumpoints).toLocaleString()}**. This server is running on **Shard ${message.guild.shardId}**`);
+        message.reply(`🧼**Soap BOT** is in **${guilds}** servers with **${users[0].countusers.toLocaleString()}** users. Total soap in hands: **🧼${economy_wallet[0].sumpoints.toLocaleString()}**. Total soap in stashes: **🧼${economy_bank[0].sumpoints.toLocaleString()}**. Total soap in hands and stashes combined: 🧼**${(economy_wallet[0].sumpoints + economy_bank[0].sumpoints).toLocaleString()}**. Total soap that can be stored in stash: **🧼${economy_max_bank[0].sumpoints.toLocaleString()}**. This server is running on **Shard ${message.guild.shardId}**`);
     }
 }
